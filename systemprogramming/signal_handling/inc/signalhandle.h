865c07779a4eb8d3461d2b27e5b2b390a647998c
nagasai128 <108229848+nagasai128@users.noreply.github.com>
2022-07-20 00:14:14 +0530

    Add files via upload

---
+++ systemprogramming/signal_handling/inc/signalhandle.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <cstring>
+#define SIZE 100
+
+using namespace std;
